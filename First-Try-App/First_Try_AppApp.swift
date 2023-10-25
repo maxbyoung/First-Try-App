66924ccdd962d92cf13d191537008db1393238f5
maxbyoung <maxbyoung102@gmail.com>
2023-10-24 23:32:36 -0600

    Initial Commit

---
+++ First-Try-App/First_Try_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_Try_AppApp.swift
+//  First-Try-App
+//
+//  Created by Max Young on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct First_Try_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
