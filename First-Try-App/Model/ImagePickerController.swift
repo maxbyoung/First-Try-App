5ec578c1eab1204a55aa982961eb4172703ffb0c
maxbyoung <maxbyoung102@gmail.com>
2023-10-28 20:40:39 -0600

    Added folder structure as well as a scroll view with the framework to add an imagepicker

---
+++ First-Try-App/Model/ImagePickerController.swift
@@ -0,0 +1,10 @@
+//
+//  ImagePickerController.swift
+//  First-Try-App
+//
+//  Created by Max Young on 10/28/23.
+//
+
+import Foundation
+import SwiftUI
+
---
+++ First-Try-App/View/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  First-Try-App
+//
+//  Created by Max Young on 10/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("FIRST TRY")
+            }
+            .navigationTitle("Feed")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Image(systemName: "plus")
+                        .foregroundColor(.black)
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
